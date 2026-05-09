aa0c84da1857834addb5b230ea7462548c9875ce
Gilbert Corrales <gcorrales@gmail.com>
2026-05-09 14:43:56 +0100

    adding setup script

---
+++ apps/scripts/setup.sh
@@ -0,0 +1,42 @@
+## Basics
+brew install --cask 1password
+brew install --cask raycast
+
+## Dev Basics
+brew install --cask github
+brew install nvm
+brew install python
+brew install uv
+
+## Editors
+brew install --cask visual-studio-code
+brew install --cask windsurf
+brew install --cask antigravity
+
+### AI Agent Apps
+brew install --cask codex-app
+
+### AI Apps
+brew install --cask claude
+brew install --cask chatgpt
+
+## Office Apps
+brew install --cask superhuman
+brew install --cask notion
+brew install --cask notion-calendar
+brew install --cask granola
+brew install --cask figma
+brew install --cask framer
+brew install --cask affinity
+brew install --cask linear-linear
+brew install --cask microsoft-office
+brew install --cask obsidian
+
+## Browsers
+brew install --cask thebrowsercompany-dia
+brew install --cask comet
+brew install --cask google-chrome
+brew install --cask chatgpt-atlas
+
+## Utils
+brew install mole
