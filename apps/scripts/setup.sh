94681672fe8000f14131ad358502b03c547ecda6
Gilbert Corrales <gcorrales@gmail.com>
2026-07-18 20:08:42 +0100

    updated setup with apps

--- apps/scripts/setup.sh
+++ apps/scripts/setup.sh
@@ -4,6 +4,7 @@ brew install --cask raycast
 
 ## Dev Basics
 brew install --cask github
+brew install gh
 brew install nvm
 brew install python
 brew install uv
@@ -13,9 +14,6 @@ brew install --cask visual-studio-code
 brew install --cask windsurf
 brew install --cask antigravity
 
-### AI Agent Apps
-brew install --cask codex-app
-
 ### AI Apps
 brew install --cask claude
 brew install --cask chatgpt
@@ -24,19 +22,20 @@ brew install --cask chatgpt
 brew install --cask superhuman
 brew install --cask notion
 brew install --cask notion-calendar
+brew install --cask obsidian
 brew install --cask granola
 brew install --cask figma
 brew install --cask framer
 brew install --cask affinity
 brew install --cask linear-linear
 brew install --cask microsoft-office
-brew install --cask obsidian
+brew install --cask microsoft-auto-update
 
 ## Browsers
 brew install --cask thebrowsercompany-dia
-brew install --cask comet
 brew install --cask google-chrome
-brew install --cask chatgpt-atlas
+brew install --cask microsoft-edge
+brew install --cask comet
 
 ## Utils
 brew install mole
